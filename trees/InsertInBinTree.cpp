1b76885dc5fe97f4a5f4cebff353369576ed6f06
Shivam Awasthi <shivam@olibr.com>
2023-06-25 23:01:08 +0530

    trees

--- trees/InsertInBinTree.cpp
+++ trees/InsertInBinTree.cpp
@@ -1,45 +1,78 @@
-// #include <iostream>
-// #include <bits/stdc++.h>
-// using namespace std;
-//     // int val;
-//     // TreeNode* left;
-//     // TreeNode* right;
-//     // TreeNode(int v){
-//     //     this->val = v;
-//     //     this->right=NULL;
-//     //     this->left=NULL;
-//     // }
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-// class Solution {
-// public:
-//     void insert(TreeNode* &root, int val){
-//         TreeNode* temp = root;
-//         if(temp->val < val && temp->right != NULL){
-//             insert(temp->right, val);
-//         }
-//         if(temp->val > val && temp->left != NULL){
-//             insert(temp->left, val);
-//         }
-//         if(temp->left == NULL && temp->right == NULL){
-//             TreeNode* t = new TreeNode(val);
-//             if(temp->val > val){
-//                 temp->left = t;
-//             }
-//             else if(temp->val < val){
-//                 temp->right = t;
-//             }
-//             return;
-//         }
-//         return;
+class node{
+    public:
+    int val;
+    node* left;
+    node* right;
+    node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
 
-//     }
-//     TreeNode* insertIntoBST(TreeNode* root, int val) {
-//            insert(root, val);
-//            return root;
-//     }
-// };
+void levelOrder(node* root){
+        queue <node*> q;
+        if(!root){
+            return;
+        }
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            node* front = q.front();
+            q.pop();
+            if(front == NULL){
+                cout<<endl;
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }
+            else{
+                if(front->left){
+                    q.push(front->left);
+                }
+                if(front->right){
+                    q.push(front->right);
+                }
+                cout<<front->val<<" ";
+            }
+        }
 
-// int main(){
-//     // Solution sol;
-//     return 0;
-// }
+    }
+
+node* buildTree(node* root){
+    cout<<"enter the data"<<endl;
+    int val;
+    cin>>val;
+    root = new node(val);
+    if(val == -1){
+        return NULL;
+    }
+    cout<<"LEFT ";
+    root->left = buildTree(root->left);
+    cout<<"RIGHT ";
+    root->right = buildTree(root->right);
+}
+
+int heightBinaryTrees(node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int left = heightBinaryTrees(root->left);
+    int right = heightBinaryTrees(root->right);
+
+    return max(left, right)+1;
+}
+
+int main(){
+    node* root;
+    root = buildTree(root);
+    levelOrder(root);
+    cout<<heightBinaryTrees(root);
+    return 0;
+}
+
+// 10 20 30 -1 -1 25 -1 -1 99 65 -1 -1 88 -1 -1
