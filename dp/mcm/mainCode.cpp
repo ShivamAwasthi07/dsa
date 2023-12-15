66dc0306572745c19bf5f52a3ef272320ec3b29e
Shivam Awasthi <awasthi@Shivams-MacBook-Air.local>
2023-12-16 01:52:39 +0530

    leetcode

---
+++ dp/mcm/mainCode.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> solve){
+    
+    int n = solve.size();
+    vector<vector<int>> dp(n, vector<int> (n, 0));
+
+}
+
+int main(){
+    vector<int> matrix;
+    
+    matrix.push_back(5);
+    matrix.push_back(4);
+    matrix.push_back(6);
+    matrix.push_back(2);
+    matrix.push_back(7);
+
+    int a = solve(matrix);
+
+    return 0;
+}
